751e81bf209a83dda156528b0cc2b6c5ec51fcae
Philipp Serr <episource@gmx.de>
2016-08-21 14:09:01 +0200

    Use shimgen extension in processhacker nupkg

--- processhacker/tools/chocolateyInstall.ps1
+++ processhacker/tools/chocolateyInstall.ps1
@@ -1,10 +1,28 @@
-. $PSScriptRoot/chocolateyCommon.ps1
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+$destdir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$arch   = "x64"
+If (Get-OSArchitectureWidth -Compare 32) {
+    $arch   = "x86"
+}
+
+$appname   = "ProcessHacker"
+$exename   = "$appname.exe"
+$startname = "$appname ($arch)"
+$x86dir    = Join-Path $destDir "x86"
+$x64dir    = Join-Path $destDir "x64"
+$bindir    = Join-Path $destDir $arch
+$exe       = Join-Path $bindir $exename
+$x86exe    = Join-Path $x86dir $exename
+$x64exe    = Join-Path $x64dir $exename
+
 
 # Install start menu shortcut
-Install-ChocolateyShortCut `
-    -ShortcutFilePath $startLink -TargetPath $exe
+Install-StartMenuLink -LinkName $startname -TargetPath $exe
 
 # Add shim to path
-Install-BinFile -Name "${appname}_x86" -Path $x86exe
-Install-BinFile -Name "${appname}_x64" -Path $x64exe
-Install-BinFile -Name "$appname"       -Path $exe
+Install-Shim -Name "${appname}_x86" -Path $x86exe
+Install-Shim -Name "${appname}_x64" -Path $x64exe
+Install-Shim -Name "$appname"       -Path $exe
--- processhacker/tools/chocolateyUninstall.ps1
+++ processhacker/tools/chocolateyUninstall.ps1
@@ -1,4 +1,11 @@
-. $PSScriptRoot/chocolateyCommon.ps1
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+$destdir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$drvname   = "kprocesshacker.sys"
+$x64dir    = Join-Path $destDir "x64"
+$drvsys    = Join-Path $x64dir $drvname
+
 
 # Try to stop KProcessHacker service (otherwise kprocesshacker.sys cannot be
 # deleted!)
@@ -8,18 +15,7 @@ $kphServiceName = "KProcessHacker$($drvsysInfo.VersionInfo.ProductMajorPart)"
 Try {
     & net stop $kphServiceName | Out-Null
 } Catch {
-    Write-Verbose `
-        "Failed to stop KPH service $service. This is fine if ProcessHacker `
-        has not been used since the last reboot."
+    Write-Warning `
+        "Failed to stop KPH service $kphServiceName. This is fine if `
+        ProcessHacker has not been used in elevated mode since the last reboot."
 }
-
-
-# Uninstall start menu shortcut
-If (Test-Path -Path $startLink) {
-    Remove-item -Path $startLink
-}
-
-# Remove shim from path
-Uninstall-BinFile -Name "${appname}_x86" -Path $x86exe
-Uninstall-BinFile -Name "${appname}_x64" -Path $x64exe
-Uninstall-BinFile -Name "$appname"       -Path $exe
