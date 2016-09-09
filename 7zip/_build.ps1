36e6492d9dbe3e20608ee9950f4dcacd3edb6293
Philipp Serr <episource@gmx.de>
2016-09-09 23:12:53 +0200

    Create a template for 7-Zip

---
+++ 7zip/_build.ps1
@@ -0,0 +1,21 @@
+# Enable common parameters
+[CmdletBinding()] Param()
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$versionInfo = Get-VersionInfoFromSourceforge `
+    -Project "sevenzip" `
+    -Filter  "/7-Zip/(?<VERSION>\d+\.\d+)/7z\d+-x64\.msi"
+$versionInfo.ReleaseNotes = Invoke-WebRequest -UseBasicParsing `
+    -Uri     "http://7-zip.org/history.txt"
+
+#Normalize version string to be semver compatible
+$versionParts = $versionInfo.Version.Split(".") | %{ [Int]$_ }
+While ($versionParts.length -lt 3) {
+    $versionParts += 0
+}
+$versionInfo.Version = [String]::Join(".", $versionParts)
+   
+   
+New-Package -VersionInfo $versionInfo
