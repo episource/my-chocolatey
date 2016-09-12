2508647fd04c0a90846d23ab38369291870b7a49
Philipp Serr <episource@gmx.de>
2016-09-12 21:05:20 +0200

    Lazy initialize release notes

--- conemu/_build.ps1
+++ conemu/_build.ps1
@@ -21,38 +21,39 @@ For ($i = 0; $i -lt 3; $i++) {
     $versionInfo.Version     += $version.Substring(2 * $i, 2).TrimStart("0")
 }
 
+# lazy initialize release notes
+$versionInfo.ReleaseNotes = {
+    $notes = ""
 
-# Query release notes
-$notes = ""
+    # -UseBasicParsing makes .Links freeze when querying _posts!?
+    $notesListResponse = Invoke-WebRequest `
+        "https://github.com/ConEmu/ConEmu.github.io/tree/master/_posts"
+    $notesUrls = $notesListResponse.Links | ?{ 
+        $_.PSObject.Properties['title'] -and $_.title -match ".*build.*\.md" `
+        -and $_.PSObject.Properties['class'] -and $_.class -eq "js-navigation-open"
+    } | Sort-Object -Property 'title' -Descending | %{
+        "https://github.com" + $_.href -replace "/blob/","/raw/" }
 
-# -UseBasicParsing makes .Links freeze when querying _posts!?
-$notesListResponse = Invoke-WebRequest `
-    "https://github.com/ConEmu/ConEmu.github.io/tree/master/_posts"
-$notesUrls = $notesListResponse.Links | ?{ 
-    $_.PSObject.Properties['title'] -and $_.title -match ".*build.*\.md" `
-    -and $_.PSObject.Properties['class'] -and $_.class -eq "js-navigation-open"
-} | Sort-Object -Property 'title' -Descending | %{
-    "https://github.com" + $_.href -replace "/blob/","/raw/" }
+    Write-Host "Collecting release notes. This might take some while."
+    $count = $notesUrls.length
+    $emptyLine = ""
+    For ($i = 0; $i -lt $count; $i++) {
+        Write-Host -NoNewline "`r$emptyLine"
+        $url = $notesUrls[$i]
+        $status = "($($i+1)/$count) $url"
+        $emptyLine = " " * $status.length
+        Write-Host -NoNewline "`r$status"
+        
+        $response = Invoke-WebRequest -UseBasicParsing -Uri $url
+        $notes   += $response.Content `
+            -replace '---\s*','' `
+            -replace 'build:','# build:'
+        $notes   += "`n"
+    }
+    Write-Host "" # quit status line (newline)
 
-Write-Host "Collecting release notes. This might take some while."
-$count = $notesUrls.length
-$emptyLine = ""
-For ($i = 0; $i -lt $count; $i++) {
-    Write-Host -NoNewline "`r$emptyLine"
-    $url = $notesUrls[$i]
-    $status = "($($i+1)/$count) $url"
-    $emptyLine = " " * $status.length
-    Write-Host -NoNewline "`r$status"
-    
-    $response = Invoke-WebRequest -UseBasicParsing -Uri $url
-    $notes   += $response.Content `
-        -replace '---\s*','' `
-        -replace 'build:','# build:'
-    $notes   += "`n"
+    return $notes
 }
-Write-Host "" # quit status line (newline)
-
-$versionInfo.ReleaseNotes = $notes
 
 
 New-Package -VersionInfo $versionInfo
