194f600ef2f5ecc09c0ffeb715ce9e6556bdc627
Philipp Serr <episource@gmx.de>
2016-09-27 19:30:08 +0200

    Create a 7+ Taskbar Tweaker configuration package

---
+++ 7plus-taskbar-tweaker-config/tools/chocolateyInstall.ps1
@@ -0,0 +1,190 @@
+$myConfig = @{
+    "SOFTWARE\7 Taskbar Tweaker" = @{
+        "hidetray"                    = 0x00000000
+        "language"                    = 0x00000407
+        "lefttrayfunc"                = 0x00000000
+        "updcheck"                    = 0x00000001
+        "updcheckauto"                = 0x00000000
+        "updchecktime"                = 0x03a43031
+        "Options"                     = @{
+            # Taskbar items > Right click: 
+            # 0 - Jump list (default); 1 - Standard window menu
+            "1318069356" = 0x00000000
+            
+            # Taskbar items > Middle click:
+            # 0 - New program instance (default)
+            # 1 - Switch to
+            # 2 - Minimize
+            # 3 - Close
+            "1318069373" = 0x00000000
+            
+            # Taskbar items > Dropping: 0 - Pin to (default); 1 - Open with
+            "1318069660" = 0x00000000
+            
+            # Taskbar items > Hovering
+            # 0 - Show thumbnail preview (default)
+            # 1 - List
+            # 2 - Tooltip
+            # 3 - Nothing
+            "1318069999" = 0x00000000
+            
+            # Taskbar items > Thumbnails > Drag to reorder: 0 - Off; 1 - On
+            "1329809759" = 0x00000001
+            
+            # Taskbar items > Thumbnails > Left click on the active thumbnail
+            # minimizes the window: 0 - Off; 1 - On
+            "1326387462" = 0x00000000
+            
+            # Taskbar items > Pinned items > Remove extra gap between items:
+            # 0 - Off; 1 - On
+            "1318070012" = 0x00000001
+            
+            # Taskbar items > Pinned items > Open with double click:
+            # 0 - Off; 1 - On
+            "1318070048" = 0x00000001
+            
+            
+            
+            # Grouping and combining > Grouping:
+            # 0 - Group by Application ID (default)
+            # 1 - Don't group
+            "1318069425" = 0x00000000
+            
+            # Grouping and combining > Grouping > When not grouping, place new
+            # items next to the existing ones: 0 - Off; 1 - On
+            "1322745792" = 0x00000000
+            
+            # Grouping and combining > Grouping > Don't group pinned items:
+            # 0 - Off; 1 - On
+            "1318069475" = 0x00000000
+            
+            # Grouping and combining > Grouping > Drag within/between groups
+            # using right mouse button: 0 - Off; 1 - On
+            "1318069940" = 0x00000000
+            
+            # Grouping and combining > Combining:
+            # 0 - Use taskbar settings (default)
+            # 1 - Combine grouped buttons
+            # 2 - Don't combine grouped buttons
+            "1321127752" = 0x00000000
+            
+            # Grouping and combining > Combining > Decombine > Active Group:
+            # 0 - Off; 1 - On
+            "1319067226" = 0x00000000
+            
+            # Grouping and combining > Combining > Decombine > On mouse hover:
+            # 0 - Off; 1 - On
+            "1318069546" = 0x00000000
+            
+            # Grouping and combining > Combining > Decombine > On mouse hover >
+            # ...and show labels: 0 - Off; 1 - On
+            "1318069553" = 0x00000000
+            
+            # Grouping and combining > Left click on combined item:
+            # 0 - Open thumbnail preview (default)
+            # 1 - Cycle through windows
+            # 2 - Open last window if inactive, otherwise open thumbnail preview
+            "1318069604" = 0x00000001
+            
+            
+            
+            # Other taskbar options > Mouse wheel > Cycle between taskbar buttons:
+            # 0 - Off; 1 - On
+            "1318069808" = 0x00000000
+            
+            # Other taskbar options > Mouse wheel > Cycle between taskbar 
+            # buttons > Skip minimized windows: 0 - Off; 1 - On
+            "1318069834" = 0x00000000
+            
+            # Other taskbar options > Mouse wheel > Minimize/restore when mouse
+            # is over > Taskbar buttons: 0 - Off; 1 - On
+            "1318069878" = 0x00000000
+            
+            # Other taskbar options > Mouse wheel > Minimize/restore when mouse
+            # is over > Thumbnails: 0 - Off; 1 - On
+            "1318069924" = 0x00000000
+            
+            # Other taskbar options > Mouse wheel > Control volume when mouse is
+            # over > The taskbar: 0 - Off; 1 - On
+            "1322639717" = 0x00000000
+            
+            # Other taskbar options > Mouse wheel > Control volume when mouse is
+            # over > The notification area: 0 - Off; 1 - On
+            "1322639729" = 0x00000000
+            
+            # Other taskbar options > Double click on empty space:
+            # 0 - Nothing (default)
+            # 1 - Show desktop
+            # 2 - Ctrl+Alt+Tab
+            # 3 - Task Manager
+            # 4 - Taskbar Inspector
+            # 5 - Toggle mute system volume
+            # 6 - Toggle taskbar auto-hidetray
+            # 7 - Win+Tab / Ctrl+Win+Tab
+            # 8 - Start menu / Start screen
+            "1318069751" = 0x00000000
+            
+            # Other taskbar options > Middle click on empty space:
+            # 0 - Nothing (default)
+            # 1 - Show desktop
+            # 2 - Ctrl+Alt+Tab
+            # 3 - Task Manager
+            # 4 - Taskbar Inspector
+            # 5 - Toggle mute system volume
+            # 6 - Toggle taskbar auto-hidetray
+            # 7 - Win+Tab / Ctrl+Win+Tab
+            # 8 - Start menu / Start screen
+            "1318069695" = 0x00000000
+            
+            # Other taskbar options > Other > Hide the Start button
+            # 0 - Off; 1 - On
+            "1383174804" = 0x00000000
+            
+            # Other taskbar options > Other > Hide the "Show desktop" button
+            # 0 - Off; 1 - On
+            "1321056220" = 0x00000000
+            
+            # Other taskbar options > Other > Display seconds on the tray clock
+            # 0 - Off; 1 - On
+            "1365090333" = 0x00000001
+            
+            # Other taskbar options > Other > Reserve empty space on taskbar
+            # 0 - Off; 1 - On
+            "1321056197" = 0x00000000
+        }
+        "OptionsEx"                   = @{
+            "always_show_thumb_labels"        = 0x00000000
+            "always_show_tooltip"             = 0x00000000
+            "cycle_same_virtual_desktop"      = 0x00000000
+            "disable_items_drag"              = 0x00000000
+            "disable_taskbar_transparency"    = 0x00000000
+            "disable_topmost"                 = 0x00000000
+            "disable_tray_icons_drag"         = 0x00000000
+            "drag_towards_desktop"            = 0x00000000
+            "fix_hang_reposition"             = 0x00000001
+            "list_reverse_order"              = 0x00000000
+            "multipage_wheel_scroll"          = 0x00000000
+            "multirow_equal_width"            = 0x00000000
+            "no_start_btn_spacing"            = 0x00000000
+            "no_width_limit"                  = 0x00000000
+            "nocheck_close"                   = 0x00000000
+            "nocheck_maximize"                = 0x00000000
+            "nocheck_minimize"                = 0x00000000
+            "pinned_ungrouped_animate_launch" = 0x00000000
+            "right_drag_toggle_labels"        = 0x00000000
+            "scroll_maximize_restore"         = 0x00000000
+            "scroll_reverse_cycle"            = 0x00000000
+            "scroll_reverse_minimize"         = 0x00000000
+            "show_desktop_button_size"        = 0x00000000
+            "show_desktop_on_hover"           = 0x00000000
+            "sndvol_tooltip"                  = 0x00000000
+            "tray_clock_fix_width"            = 0x00000000
+            "tray_icons_padding"              = 0x00000000
+            "virtual_desktop_order_fix"       = 0x00000000
+            "w10_large_icons"                 = 0x00000000
+            "w7_show_desktop_classic_corner"  = 0x00000000
+            "w7_tasklist_htclient"            = 0x00000000
+        }
+    }
+}
+Install-UserProfileRegistryImage -Image $myConfig -Force
