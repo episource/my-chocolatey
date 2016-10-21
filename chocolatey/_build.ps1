f27a2c12b7372f3ca19fde3342a2595ccd95a2fc
Philipp Serr <episource@gmx.de>
2016-10-21 22:45:03 +0200

    Mirror the main chocolatey package

---
+++ chocolatey/_build.ps1
@@ -0,0 +1,30 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+#
+# This script downloads a prebuild nupkg from the chocolatey galery
+#
+
+# Determine latest version
+$versions = Invoke-Webrequest -UseBasicParsing `
+        "https://chocolatey.org/api/v2/package-versions/chocolatey" |
+    Select-Object -Expand Content  | ConvertFrom-Json
+$newestVersion = $versions[-1]
+
+# Check if the package has already been downloaded
+$nupkgFile = Get-Item "$global:CFRepository/chocolatey.$newestVersion.nupkg" `
+    -ErrorAction SilentlyContinue
+If ($nupkgFile) {
+    Write-Verbose "Existing package found: $nupkgFile"
+    return $nupkgFile
+}
+
+$vtApiKey = Get-Variable "CFVtApiKey" -ErrorAction SilentlyContinue
+$nupkgUrl = "https://chocolatey.org/api/v2/package/chocolatey/$newestVersion"
+If ($vtApiKey) {
+    Get-WebFile -Url $nupkgUrl -OutFile $global:CFBuildRoot -VtApiKey $vtApiKey
+} Else {
+    Get-WebFile -Url $nupkgUrl -OutFile $global:CFBuildRoot
+}
