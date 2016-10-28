53ed6a736700a03eca38082b028dc3339bd8331b
Philipp Serr <episource@gmx.de>
2016-10-28 22:11:11 +0200

    Create a template for VLC video player

---
+++ vlc/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+
+$exe64 = Get-Item "$toolsDir/vlc-$env:chocolateyPackageVersion/vlc.exe"
+Install-StartMenuLink -LinkName "VLC" -TargetPath $exe64
+Set-AutoShim -Pattern $exe64 -Invert -Mode Ignore | Out-Null
