a895b6f8636a36efe5125e782bb9aea48d7ed423
Philipp Serr <episource@gmx.de>
2016-11-05 22:55:28 +0100

    Enable strict mode set ErrorAction to stop

--- _template/_build.ps1
+++ _template/_build.ps1
@@ -1,5 +1,9 @@
 ﻿# Enable common parameters
 [CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
 # Import my-chocolatey config & modules
 . $PSScriptRoot/../_root.ps1
 
--- _template/tools/chocolateyInstall.ps1
+++ _template/tools/chocolateyInstall.ps1
@@ -1,4 +1,8 @@
-﻿$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 # Note: this file should be encoded as "UTF8 with BOM"
 Throw "Not implemented"
