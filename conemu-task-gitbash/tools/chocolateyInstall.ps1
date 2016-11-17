926d68468cb37e66cf9bb59a0c09f83a8d8a40cf
Philipp Serr <episource@gmx.de>
2016-11-17 22:28:15 +0100

    Remove ConEmu from git-bash PATH

--- conemu-task-gitbash/tools/chocolateyInstall.ps1
+++ conemu-task-gitbash/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $taskName  = "{Dev::Git Bash}"
         "_SortKey" = "410"
         "Active"   = 0x00000000
         "GuiArgs"  = "/icon $gitIcon"
-        "Cmd1"     = "set PATH=""$gitBinDir;%PATH%"" & $bashExe --login -i"
+        "Cmd1"     = "call set PATH=^%PATH:%ConEmuDir%=XXX^% & C:\ProgramData\chocolatey\lib\git\tools\git-cmd.exe --no-cd --command=usr/bin/bash.exe --login -i"
         "Count"    = 0x00000001
         "Flags"    = 0x00000000
         "Hotkey"   = 0x00000000
