25764ea99fbed05a026eb3f4d7f673f510352162
Philipp Serr <episource@gmx.de>
2016-11-30 21:04:20 +0100

    Create a template for git-merge-subtree2

---
+++ git-merge-subtree2/_build.ps1
@@ -0,0 +1,17 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$repo = "episource/git-merge-subtree2"
+$filename = "git-merge-subtree2.zip"
+
+Get-VersionInfoFromGithub -Repo $repo -File $filename `
+    | Add-ChecksumFromGithubAsset -Algorithm "sha256" `
+        -ChecksumFileRegex "$filename.sha256" `
+    | New-Package
