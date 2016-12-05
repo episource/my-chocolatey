d0ca83fb6861b86674acebea529e19d66a17b2f6
Philipp Serr <episource@gmx.de>
2016-12-05 19:28:34 +0100

    Fix removal of ConEmu directories from the PATH

--- conemu-task-gitbash/tools/chocolateyInstall.ps1
+++ conemu-task-gitbash/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ $taskName       = "{Dev::Git Bash}"
         "_SortKey" = "410"
         "Active"   = 0x00000000
         "GuiArgs"  = "/icon $gitIcon"
-        "Cmd1"     = "call set PATH=""^%PATH:%ConEmuDir%=XXX^%"" & set PATH=""%PATH%;$gitSubtree2Dir;"" & $gitDir/git-cmd.exe --no-cd --command=usr/bin/bash.exe --login -i"
+        "Cmd1"     = "set PATH=%PATH%;$gitSubtree2Dir; & call set PATH=^%PATH:%ConEmuDir%=XXX^% & $gitDir/git-cmd.exe --no-cd --command=usr/bin/bash.exe --login -i"
         "Count"    = 0x00000001
         "Flags"    = 0x00000000
         "Hotkey"   = 0x00000000
