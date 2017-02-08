7159112dc821c4714c76f6bc70e7932a5c3ee85a
Philipp Serr <episource@gmx.de>
2017-02-08 18:48:11 +0100

    Add package ilspy

---
+++ ilspy/_build.ps1
@@ -0,0 +1,17 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query github API to get url and version of the latest release
+$repo = 'icsharpcode/ILSpy'
+$filename = "ILSpy_Master_\d+(\.\d+){1,3}_Binaries\.zip"
+
+
+# Export the package (subject to _config.ps1)
+Get-VersionInfoFromGithub -Repo $repo -File $filename -EnableRegex | New-Package
---
+++ ilspy/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$exe      = Join-Path $toolsDir "ILSpy.exe"
+Install-StartMenuLink -LinkName "ILSpy" -TargetPath $exe
