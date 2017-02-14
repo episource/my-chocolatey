8ad9eeef2febbaf216e482648869095f95ff38b4
Philipp Serr <episource@gmx.de>
2017-02-14 20:49:57 +0100

    Add package XnView MP

---
+++ xnviewmp/_build.ps1
@@ -0,0 +1,40 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Parameters for parsing the download page
+$downloadPageUrl = "http://www.xnview.com/en/xnviewmp/"
+$versionRegex    = 'Download.+XnView MP (?<VERSION>\d+(?:\.\d+){1,2})'
+$hrefRegex       = '<a.*href="(?<HREF>[^"]+)".*>.*Zip Win 64bit.*</a>'
+
+# Query download page
+$htmlResponse    = Invoke-WebRequest -UseBasicParsing -Uri $downloadPageUrl
+
+If (-not ($htmlResponse.Content -match $versionRegex)) {
+    Write-Error "Failed to parse XnView MP download page - version not found"
+    return
+}
+$versionParts = @($Matches.VERSION.Split(".") | %{ [Int]$_ })
+While ($versionParts.length -lt 3) {
+    $versionParts += 0
+}
+$version = [String]::Join(".", $versionParts)
+
+If (-not ($htmlResponse.Content -match $hrefRegex)) {
+    Write-Error "Failed to parse XnView MP download page - download link not found"
+    return
+}
+# append version => unique url for proper caching
+$href = $Matches.HREF + "?version=$version"
+
+# Build the package
+@{
+    Version  = $version
+    FileUrl  = $href
+} | New-Package
---
+++ xnviewmp/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$exe      = Join-Path $toolsDir "XnViewMP\xnviewmp.exe"
+Install-StartMenuLink -LinkName "XnView MP" -TargetPath $exe
