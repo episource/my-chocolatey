437bc23f0c011c9eb92263cfb1fad4f0b5f0e7d3
Philipp Serr <episource@gmx.de>
2017-02-20 14:17:51 +0100

    Add package Greenshot
    Greenshot is a light-weight screenshot software tool for Windows with the following key features:
     * Quickly create screenshots of a selected region, window or fullscreen; you can even capture complete (scrolling) web pages from Internet Explorer.
     * Easily annotate, highlight or obfuscate parts of the screenshot.
     * Export the screenshot in various ways: save to file, send to printer, copy to clipboard, attach to e-mail, send Office programs or upload to photo sites like Flickr or Picasa, and others.
    ...and a lot more options simplyfying creation of and work with screenshots every day.
    
    Being easy to understand and configurable, Greenshot is an efficient tool for project managers, software developers, technical writers, testers and anyone else creating screenshots.

---
+++ greenshot/_build.ps1
@@ -0,0 +1,25 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query github API to get url and version of the latest release
+$repo = 'greenshot/greenshot'
+$distfile = "Greenshot-NO-INSTALLER-[0-9\.]+-RELEASE\.zip"
+$notesfile = "readme.txt"
+
+# Retrieve version info
+$vi = Get-VersionInfoFromGithub -Repo $repo -File $distfile -EnableRegex `
+    -ExtractVersionHook { $_.tag_name -replace "^Greenshot-RELEASE-" }
+$notesurl = $vi.GithubRelease.assets | ?{ $_.name -eq $notesfile } |
+    Select-Object -First 1 -ExpandProperty browser_download_url
+$response = Invoke-Webrequest -UseBasicParsing $notesurl
+$vi.ReleaseNotes = [System.Text.Encoding]::UTF8.GetString($response.Content)
+
+# Build the package
+New-Package $vi
---
+++ greenshot/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# Enable per-user configuration
+Remove-Item "$toolsDir/greenshot.ini"
+
+# Start Menu
+$exe = Get-Item "$toolsDir/Greenshot.exe"
+Install-StartMenuLink -LinkName "Greenshot" -TargetPath $exe
+
+# Autostart
+$autostart = @{
+    "SOFTWARE\Microsoft\Windows\CurrentVersion\Run" = @{
+        "Greenshot" = """$($exe.FullName)"""
+    }
+}
+Install-UserProfileRegistryImage -Image $autostart -Force
