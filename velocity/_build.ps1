e57af28f7698ce5f15e9ffa2b35fdb0129e9d76f
Philipp Serr <episource@gmx.de>
2017-02-20 09:55:39 +0100

    Add package Velocity
    Velocity gives your Windows desktop offline access to over 150 API documentation sets
    
    Over 150+ API documentation sets are available in Velocity covering some of the most popular languages and API frameworks that you use for everyday development. If you've every seen or used Dash on the Mac then you have a good idea of what Velocity is except for Windows. The very same docsets you use in Dash are available with Velocity too.

---
+++ velocity/_build.ps1
@@ -0,0 +1,26 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query download page
+$downloadPageUrl = "http://velocity.silverlakesoftware.com/"
+$response = Invoke-WebRequest -UseBasicParsing -Uri $downloadPageUrl
+$href = $response.links | ?{
+    Set-StrictMode -Off; $_.id -ieq "downloadbutton"
+} | %{ $_.href }
+
+# Extract version
+$href -match "VelocitySetup-(?<VERSION>\d+\.\d+\.\d+)" | Out-Null
+$version = $Matches.Version
+
+# Build the package
+@{
+    Version  = $version
+    FileUrl  = $href
+} | New-Package
---
+++ velocity/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+# Extract msi
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$msi      = Get-Item "$toolsDir\VelocitySetup*.msi"
+$pkg      = Move-Item $msi "$toolsDir\.." -PassThru
+
+Start-Process -Wait msiexec @(
+    "/a", $pkg, "/passive", "/qn", "TARGETDIR=$toolsDir",
+    "/liwe", "$toolsDir\msi.log"
+)
+
+Remove-Item $msi
+Remove-Item $pkg
+
+# Add startmenu entry
+$exe      = Join-Path $toolsDir "Silverlake Software LLC\Velocity\Velocity.exe"
+Install-StartMenuLink -LinkName "Velocity" -TargetPath $exe
