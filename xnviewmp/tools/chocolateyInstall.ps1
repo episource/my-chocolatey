a2919fcb50fba96944e1b5bb715084ac3fe724b5
Philipp Serr <episource@gmx.de>
2017-03-08 19:57:47 +0100

    Remove unnecessary shims from xnviewmp package

--- xnviewmp/tools/chocolateyInstall.ps1
+++ xnviewmp/tools/chocolateyInstall.ps1
@@ -4,4 +4,5 @@ $ErrorAction = "Stop"
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $exe      = Join-Path $toolsDir "XnViewMP\xnviewmp.exe"
+Set-AutoShim -Pattern $exe -Invert -Mode Ignore | Out-Null
 Install-StartMenuLink -LinkName "XnView MP" -TargetPath $exe
