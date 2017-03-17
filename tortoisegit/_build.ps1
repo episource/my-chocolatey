0472cb02bb409ccdb69d5b54aa7777601766d511
Philipp Serr <episource@gmx.de>
2017-03-17 20:18:27 +0100

    Update tortoisegit to find hotfix releases
    Previously tortoisegit 2.4.0.2 has not been found.

--- tortoisegit/_build.ps1
+++ tortoisegit/_build.ps1
@@ -4,33 +4,28 @@
 . $PSScriptRoot/../_root.ps1
 
 
-$downloadIndexUrl = "https://download.tortoisegit.org/tgit/"
-$notesBaseUrl     = 
-$versionUrlRegex  = "^(?<VERSION>(\d+\.){2,3}\d+)/?$"
-$htmlResponse     = Invoke-WebRequest -UseBasicParsing -Uri $downloadIndexUrl
+# Find download url + version
+$downloadPageUrl     = "https://tortoisegit.org/download/"
+$downloadPageContent = Invoke-WebRequest -UseBasicParsing -Uri $downloadPageUrl
+
+$msiUrl = $DownloadPageContent.Links | ?{
+    $_.href -match "TortoiseGit-(?<VERSION>(?:\d+\.){2,3}\d+)-64bit\.msi$" 
+} | %{ $_.href -replace "^//","http://" } | Select-Object -First 1
+$version = $Matches.VERSION
 
 
-$version = $htmlResponse.Links | %{
-        If ($_.href -match "^(?<VERSION>(\d+\.){2,3}\d+)/?$") {
-            write-output $Matches.VERSION 
-        } 
-    } | Sort-Object -Property @{ Expression = { [Version]$_ } } -Descending |
-    Select-Object -First 1
-
-
-$fileUrl   = "$downloadIndexUrl/$version/TortoiseGit-$version-64bit.msi"
-$notesUrl  = "https://tortoisegit.org/docs/releasenotes/#Release_$version"
-
-$notesHtml = Invoke-WebRequest -UseBasicParsing -Uri $notesUrl
-$notesHtml -match `
+# Prepare release notes
+$releaseNotesUrl     = "https://tortoisegit.org/docs/releasenotes/"
+$releaseNotesContent = Invoke-WebRequest -UseBasicParsing -Uri $notesUrl
+$releaseNotesContent -match `
     "(?is)<pre[^>]*>\s*(?<RELEASENOTES><strong\s*id=""?Release.+)</pre>" | Out-Null
-$notes     = $Matches.RELEASENOTES -replace `
+$releaseNotes = $Matches.RELEASENOTES -replace `
     '<[^>]+>(?<INNERHTML>[^<]+)</[^>]+>','${INNERHTML}'
     
     
 New-Package -VersionInfo @{
     Version      = $version
-    FileUrl      = $fileUrl
+    FileUrl      = $msiUrl
     Checksum     = $null
-    ReleaseNotes = $notes
+    ReleaseNotes = $releaseNotes
 }
