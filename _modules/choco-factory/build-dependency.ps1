1d1312379f8efdf5b2cc3e55376425cb9b1e67bd
Philipp Serr <episource@gmx.de>
2017-03-19 10:30:32 +0100

    Add build-time dependencies
    The cmdlet Select-BuildDependency has been added to choco-factory to
    support build-time dependencies.

---
+++ _modules/choco-factory/build-dependency.ps1
@@ -0,0 +1,98 @@
+# Copyright 2016 Philipp Serr (episource)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+
+<#
+.SYNOPSIS
+    Ensure that a build-time dependency is installed and return the path of it's
+    package directory.
+
+.DESCRIPTION
+    This function is used to ensure that build-time dependencies (chocolatey
+    packages) are installed. It also returns the associated package directory.
+    
+    An error is raised if the package isn't found.
+        
+.PARAMETER Name
+    The name of the chocolatey package the calling build script depends on.
+           
+.OUTPUT
+    The package directory as FileSystemInfo object.
+           
+.EXAMPLE
+    TODO
+#>
+function Select-BuildDependency {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ParameterSetName="MinVersion", Mandatory=$true,
+                   ValueFromPipeline=$true
+                   ,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName="ExactVersion", Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [String] $Name,
+        
+        [Parameter(ParameterSetName="MinVersion", Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [String] $MinVersion = $null,
+        
+        [Parameter(ParameterSetName="ExactVersion", Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [String] $ExactVersion = $null
+    ) 
+    
+    Begin {
+        $chocoCmd = Get-Command choco
+        $chocoDir = Get-Item "$($chocoCmd.Path)/../../"
+        
+        $versionComparer = [VersionComparer]::new()
+    }
+    Process {
+        $pkgDir = "$chocoDir/lib/$Name"
+        $pkgNuspec = "$pkgdir/$Name.nuspec"
+        switch ($PsCmdlet.ParameterSetName) {
+            "MinVersion" { $dependencyDesc = "$Name>=$MinVersion" }
+            "ExactVersion" { $dependencyDesc = "$Name=$ExactVersion" }
+        }
+                
+        If (-not (Test-Path -Type Leaf $pkgNuspec)) {
+            Write-Error "Dependency $dependencyDesc not installed!"
+            return
+        }
+        
+        $actualVersion = $(_Get-NuspecIdAndVersion($pkgNuspec)).Version
+        switch ($PsCmdlet.ParameterSetName) {
+            "MinVersion" { 
+                $versionOk = $versionComparer.Compare(
+                    $actualVersion, $MinVersion) -gt 0 
+            }
+            "ExactVersion" {
+                $versionOk = $versionComparer.Compare(
+                    $actualVersion, $ExactVersion) -eq 0 
+            }
+        }
+        
+        
+        If (-not $versionOk) {
+            Write-Error "Found $Name=$actualVersion, but wanted $dependencyDesc!"
+            return
+        }        
+        
+        Write-Output $(Get-Item $pkgDir)
+    }
+}
--- _modules/choco-factory/choco-factory.psd1
+++ _modules/choco-factory/choco-factory.psd1
@@ -19,7 +19,7 @@
 RootModule = 'choco-factory.psm1'
 
 # Version number of this module.
-ModuleVersion = '2017.03.02-SNAPSHOT'
+ModuleVersion = '2017.03.02'
 
 # ID used to uniquely identify this module
 GUID = 'ff3deb25-4873-4bd2-9bfc-71fdddd7df14'
@@ -121,7 +121,8 @@ PrivateData = @{
 
         # ReleaseNotes of this module
         ReleaseNotes = @"
-v2017.03.02-SNAPSHOT
+v2017.03.02 - Add Select-BuildDependency to add build-time dependencies to
+              template build scripts
             - Add http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd to
               the list of supported nuspec namespaces
 v2017.03.01 - Add optional property FileUrlCookies to the VersionInfo structure:
