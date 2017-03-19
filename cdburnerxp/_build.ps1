983c21d36d91ae2d7b58c37bfc20f3fef8609b99
Philipp Serr <episource@gmx.de>
2017-03-19 19:05:29 +0100

    Add package cdburnerxp

---
+++ cdburnerxp/_build.ps1
@@ -0,0 +1,32 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+# Determine latest version + file url
+$dlIndexUrl = "https://cdburnerxp.se/en/download"
+$dlIndexRaw = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+$dlIndexRaw -match "(?<ZIPURL>https://download.cdburnerxp.se/portable/CDBurnerXP-x64-(?<VERSION>(?:\d+\.){2,3}\d+).zip)" | Out-Null
+$zipUrl = $Matches.ZIPURL
+$version = $Matches.VERSION
+
+# Extract changelog
+$changelogUrl = "https://cdburnerxp.se/en/development?full"
+$changelogRaw = Invoke-WebRequest -UseBasicParsing $changelogUrl
+$changelogRaw -match "(?si)(?<CHANGES><b>Version $version.*)<h1>Development log</h1>" | Out-Null
+$changes = $Matches.CHANGES `
+    -replace '(?si)<b>(Version[^<]*)</b>[^<]*<ul class="vers">','= ${1} =' `
+    -replace '[ \x09]*<li><div>(.)</div>','  ${1}' `
+    -replace '<a[^>]*>([^<]*)</a>','${1}' `
+    -replace '</li>','' `
+    -replace '</?ul[^>]*>',''
+    
+New-Package -VersionInfo @{
+    Version = $version
+    FileUrl = $zipUrl
+    ReleaseNotes = $changes
+}
---
+++ cdburnerxp/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+
+$withShim = @( "cdbxpcmd.exe", "cdbxpp.exe" )
+Set-AutoShim -Pattern $withShim -Invert -Mode Ignore | Out-Null
+Install-StartMenuLink -LinkName "CDBurnerXP" -TargetPath "$toolsDir/cdbxpp.exe"
+
+
+# store settings per use / not in the application folder
+Set-Content -Path "$toolsDir/Config.ini" -Value @"
+[CDBurnerXP]
+Portable=0
+"@
