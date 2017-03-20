cfacb4c0d594c89a48c215f3ad673169eddaa93d
Philipp Serr <episource@gmx.de>
2017-03-20 19:09:53 +0100

    Add package speedcrunch

---
+++ speedcrunch/_build.ps1
@@ -0,0 +1,31 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$dlIndexUrl = "http://speedcrunch.org/download.html"
+$dlIndexRaw = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+
+# don't use the portable build - prefer the installer: the config file location
+# is hardcoded at build-time (see settings.cpp) - only the installer build
+# uses AppData. Lateron 7zip is used to extract the installer's content.
+$dlIndexRaw -match "(?<FILEURL>https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-(?<VERSION>(?:\d+\.){1,3}\d+)-win32.exe)" | Out-Null
+$zipUrl = $Matches.FILEURL
+
+# expand version
+$versionParts = $Matches.VERSION.Split('.')
+While ($versionParts.length -lt 3) {
+    $versionParts += 0
+}
+$version = [String]::Join(".", $versionParts)
+
+
+New-Package -VersionInfo @{
+    Version = $version
+    FileUrl = $zipUrl
+}
---
+++ speedcrunch/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerExe = Get-Item "$toolsDir/SpeedCrunch*.exe"
+Get-ChocolateyUnzip -FileFullPath $installerExe.FullName -Destination $toolsDir
+
+
+# Remove installer + installer internals that have just been extracted
+# Note: Somehow Uninstall.exe doesn't get extracted when using Get-ChocolateyUnzip
+# => try to delete anyway
+Remove-Item $installerExe
+Remove-Item "$toolsDir/Uninstall.exe" -ErrorAction SilentlyContinue
+Remove-Item -Recurse "$toolsDir/`$PLUGINSDIR"
+
+
+# Start menu
+Install-StartMenuLink -LinkName "SpeedCrunch" -TargetPath "$toolsDir/speedcrunch.exe"
