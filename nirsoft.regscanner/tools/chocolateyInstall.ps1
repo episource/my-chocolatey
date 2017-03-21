403b21a7e48b4a68f0410bcba0db1837b8fd903e
Philipp Serr <episource@gmx.de>
2017-03-21 20:43:40 +0100

    Add package nirsoft.regscanner

---
+++ nirsoft.regscanner/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+Install-StartMenuLink -LinkName "NirSoft\RegScanner" -TargetPath "$toolsDir/RegScanner.exe"
