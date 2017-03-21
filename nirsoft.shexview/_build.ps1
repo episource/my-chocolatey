c6669c243fc6091c61bff75a8ad9dad389ae9267
Philipp Serr <episource@gmx.de>
2017-03-21 19:33:13 +0100

    Add package nirsoft.shexview

---
+++ nirsoft.shexview/_build.ps1
@@ -0,0 +1,14 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+New-Package -VersionInfo @{
+    Version = "file:tools/shexview.exe"
+    FileUrl = "http://www.nirsoft.net/utils/shexview-x64.zip"
+}
