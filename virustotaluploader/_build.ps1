3199b8dafe6eda38995fe6fe9ad4299c52866f36
Philipp Serr <episource@gmx.de>
2017-03-26 12:56:49 +0200

    Add package virustotaluploader

---
+++ virustotaluploader/_build.ps1
@@ -0,0 +1,26 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$dlIndexUrl = "https://www.virustotal.com/de/documentation/desktop-applications/virustotal-uploader"
+$dlIndexRaw = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+$dlIndexRaw -match "/static/bin/(?<FNAME>vtuploader(?<VERSION>(?:\d+\.){1,3}\d+)\.exe)" | Out-Null
+
+$fileUrl = "https://www.virustotal.com/static/bin/$($Matches.FNAME)"
+
+$versionParts = $Matches.VERSION.Split(".")
+while ($versionParts.Length -lt 3) {
+    $versionParts += "0"
+}
+$version = [String]::Join(".", $versionParts)
+
+New-Package -VersionInfo @{
+    Version = $version
+    FileUrl = $fileUrl
+}
---
+++ virustotaluploader/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$installerExe = Get-Item "$toolsDir/*.exe"
+Get-ChocolateyUnzip -FileFullPath $installerExe.FullName -Destination $toolsDir
+Remove-Item $installerExe
+Remove-Item "$toolsDir/uninstall.exe"
+
+$exePath = $( Get-Item "$toolsDir\VirusTotalUploader*.exe" ).FullName
+$contextMenu = @{
+    "HKLM:\SOFTWARE\Classes\*\shell\chocolatey.vtuploader" = @{
+        "(Default)" = "Send to VirusTotal"
+        "Icon" = "$exePath,0"
+        "command" = @{
+            "(Default)" = "$exePath ""%1"""
+        }
+    }
+}
+Install-RegistryImage -debug -verbose $contextMenu
