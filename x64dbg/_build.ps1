1fac7218f563298020acc9478b881d07c98cffca
Philipp Serr <episource@gmx.de>
2017-03-26 17:43:06 +0200

    Add package x64dbg

---
+++ x64dbg/_build.ps1
@@ -0,0 +1,26 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Note: There's only "one" github release, whose assets get updated frequently
+$apiArgs = @{ ApiEndpoint = "/repos/x64dbg/x64dbg/releases" }
+$apiToken = Get-Variable "CFGithubToken" -ErrorAction SilentlyContinue
+If ($apiToken) {
+    $apiArgs.ApiToken = $apiToken.Value
+}
+$release = $( Invoke-GithubApi @apiArgs )[0]
+$asset = $release.assets | 
+    Sort-Object -Property name -Descending |
+    Select-Object -First 1
+    
+$asset.Name -match "snapshot_(?<DATE>\d+-\d+-\d+)_(?<TIME>\d+-\d+).zip" 
+New-Package -VersionInfo @{
+    Version = "$($Matches.DATE -replace "-",".").$($Matches.TIME -replace "-")" -replace "\.0+","."
+    FileUrl = $asset.browser_download_url
+}
---
+++ x64dbg/tools/chocolateyInstall.ps1
@@ -0,0 +1,31 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+
+Set-AutoShim -Pattern "**" -Mode Ignore | Out-Null
+Install-StartMenuLink -LinkName "x64dbg (x86)" -TargetPath "$toolsDir\release\x32\x32dbg.exe"
+Install-StartMenuLink -LinkName "x64dbg (x64)" -TargetPath "$toolsDir\release\x64\x64dbg.exe"
+
+# create world-writable settings at an central place
+$settingsDir = "$env:ProgramData/x64dbg"
+New-Item -Type Directory "$env:ProgramData/x64dbg" -ErrorAction SilentlyContinue
+$acl = Get-Acl $settingsDir
+$usersFullControlAr = [System.Security.AccessControl.FileSystemAccessRule]::new("BUILTIN\Users", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+$acl.SetAccessRule($usersFullControlAr)
+Set-Acl $settingsDir $acl
+
+@( "x64", "x32" ) | % {
+    $platform = $_
+    
+    New-Item -Type Directory "$settingsDir/symbols" -ErrorAction SilentlyContinue
+    New-Item -Path "$toolsDir/release/$platform/symbols" -ItemType SymbolicLink -Value "$settingsDir/symbols"
+
+    @( "x64dbg.ini", "snowman.ini" ) | %{
+        $iniSrc = "$toolsDir/release/$platform/$_"
+        $iniTgt = "$settingsDir/$(Split-Path -Leaf $iniSrc)"
+        
+        "" >> $iniTgt
+        New-Item -Path $iniSrc -ItemType SymbolicLink -Value $iniTgt
+    }
+}
