8a996006726ee4523ce1d4842c179164f7be668f
Philipp Serr <episource@gmx.de>
2017-03-26 13:11:30 +0200

    Update template

--- _template/tools/chocolateyInstall.ps1
+++ _template/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿Set-StrictMode -Version latest
 $ErrorAction = "Stop"
-
-
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
 
 # Note: this file should be encoded as "UTF8 with BOM"
 Throw "Not implemented"
