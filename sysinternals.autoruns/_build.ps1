ba247aff19218837d300baeefdb3913bc31474ce
Philipp Serr <episource@gmx.de>
2017-03-28 18:37:04 +0200

    Add package sysinternals.autoruns

---
+++ sysinternals.autoruns/_build.ps1
@@ -0,0 +1,28 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$startTitle = "Autoruns"
+$exeName = "autoruns.exe"
+
+
+New-Package -VersionInfo @{
+    Version = "file:tools/$exeName"
+} -PrepareFilesHook {
+    Import-PackageResource -Url "https://live.sysinternals.com/$exeName"
+    
+    Add-Content "$($_.BuildDir)/tools/chocolateyInstall.ps1" @"
+Set-StrictMode -Version latest
+`$ErrorAction = "Stop"
+
+
+`$toolsDir = "`$(Split-Path -Parent `$MyInvocation.MyCommand.Definition)"
+Install-StartMenuLink -LinkName "Sysinternals\Sysinternals $startTitle" -TargetPath "`$toolsDir/$exeName"  
+"@
+} 
