0f5654dafcb70a68ee28e4dee932d185b441b8cc
Philipp Serr <episource@gmx.de>
2017-04-08 00:06:41 +0200

    Add package npcap

---
+++ npcap/_build.ps1
@@ -0,0 +1,29 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$apiArgs = @{ ApiEndpoint = "/repos/nmap/npcap/releases/latest" }
+$apiToken = Get-Variable "CFGithubToken" -ErrorAction SilentlyContinue
+If ($apiToken) {
+    $apiArgs.ApiToken = $apiToken.Value
+}
+$release = Invoke-GithubApi @apiArgs
+
+$distVersion = $release.tag_name.TrimStart("v")
+$versionParts = $distVersion.Split(".")
+While ($versionParts.Length -lt 3) {
+    $versionParts += "0"
+}
+$version = [String]::Join(".", $versionParts)
+
+
+New-Package -VersionInfo @{
+    Version = $version
+    Fileurl = "https://nmap.org/npcap/dist/npcap-$distVersion.exe"
+}
---
+++ npcap/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$exeInstaller = Get-Item "$toolsDir/npcap-*.exe"
+
+# /S : silent install
+# npf_startup=yes : start driver at boot time
+# loopback_support=yes : capture loopback traffic
+# dlt_null=yes : don't fake ethernet frames when capturing loopback traffic - requires capturing software support (current wireshark supports this)
+# admin_only=no : all users can capture packets
+# dot11_support=yes : enable capturing of raw 802.11 (wlan) traffic (see https://wiki.wireshark.org/CaptureSetup)
+# vlan_support=yes : support capturing tagged ethernet frames
+# winpcap_mode=yes : install winpcap compatible interface
+$instArgs = "/S /npf_startup=yes /loopback_support=yes /dlt_null=yes /admin_only=no /dot11_support=yes /vlan_support=yes /winpcap_mode=yes"
+Install-ChocolateyInstallPackage -PackageName $env:chocolateyPackageName `
+    -File $exeInstaller -FileType 'exe' -silentArgs $instArgs 
