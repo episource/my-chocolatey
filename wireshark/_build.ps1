4d673c788d4956fcdd4c2164d0628e65ecb5908a
Philipp Serr <episource@gmx.de>
2017-04-08 00:08:31 +0200

    Add package wireshark

---
+++ wireshark/_build.ps1
@@ -0,0 +1,19 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$dlIndexUrl = "https://1.eu.dl.wireshark.org/win64/"
+$dlIndex = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+$versionInfo = $dlIndex.Links | %{ 
+    If ($_.href -match "Wireshark-win64-(?<VERSION>(?:\d+\.){2,3}\d+).exe") {
+        return @{ FileUrl="$dlIndexUrl$($_.href)"; Version=$Matches.VERSION } 
+    } 
+} | ConvertTo-SortedByVersion -Property "Version" -Descending | Select-Object -First 1
+
+New-Package -VersionInfo $versionInfo
---
+++ wireshark/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+
+$installerExe = Get-Item "$toolsDir/Wireshark-win64-*.exe"
+Get-ChocolateyUnzip -FileFullPath $installerExe.FullName -Destination $toolsDir
+Remove-Item $installerExe
+Remove-Item "$toolsDir/uninstall.exe"
+Remove-Item -Recurse "$toolsDir/`$PLUGINSDIR"
+
+$withShim = @( 
+    "Wireshark.exe", "capinfos.exe", "dumpcap.exe", "editcap.exe",
+    "mergecap.exe", "rawshark.exe", "reordercap.exe", "text2pcap.exe",
+    "tshark.exe" 
+)
+Set-AutoShim -Pattern $withShim -Invert -Mode Ignore | Out-Null
+Install-StartMenuLink -LinkName "Wireshark" -TargetPath "$toolsDir\Wireshark.exe"
