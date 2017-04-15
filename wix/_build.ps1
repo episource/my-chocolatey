ec1ccdb6480100bbf72d9b7baab93c8968f217c7
Philipp Serr <episource@gmx.de>
2017-04-15 18:51:52 +0200

    Create package wix

---
+++ wix/_build.ps1
@@ -0,0 +1,27 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$rootUrl = "http://wixtoolset.org"
+$dlMainIndexUrl = "$rootUrl/releases/"
+$dlMainIndex = Invoke-WebRequest -UseBasicParsing $dlMainIndexUrl
+$dlMainIndex -match "(?s)<h2>Archived Builds</h2>.*<ul>.*?<a href=""(?<INDEX_URL>[^""]+)""" | Out-Null
+
+$dlIndexUrl = "$rootUrl$($Matches.INDEX_URL)"
+$dlIndex = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+
+$dlIndex -match "<title>(?<VERSION>.*)</title>" | Out-Null
+$version = $Matches.VERSION.TrimStart("v")
+$dlIndex -match "href=""(?<FILE_URL>.*-binaries.zip)""" | Out-Null
+$fileUrl = "$rootUrl$($Matches.FILE_URL)"
+
+New-Package -VersionInfo @{
+    Version = $version
+    FileUrl = $fileUrl
+}
---
+++ wix/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+
+Install-StartMenuLink -LinkName "Wix Toolset" -TargetPath "$toolsDir"
+Set-AutoShim -Pattern "tools/sdk/**" -Mode Ignore | Out-Null
