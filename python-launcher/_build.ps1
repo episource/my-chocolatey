53902ee618c33b56063e22c09b6e1a0afb68ec86
Philipp Serr <episource@gmx.de>
2017-04-16 17:32:39 +0200

    Add package python-launcher

---
+++ python-launcher/_build.ps1
@@ -0,0 +1,19 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$dlIndexUrl = "https://www.python.org/downloads/windows/"
+$dlIndex = Invoke-WebRequest -UseBasicParsing $dlIndexUrl
+$dlIndex -match "Latest Python 3 release - Python (?<VERSION>(?:\d+\.){2,3}\d+)" | Out-Null
+$version = $Matches.VERSION
+
+New-Package -VersionInfo @{
+    Version =  $version
+    FileUrl = "https://www.python.org/ftp/python/$version/amd64/launcher.msi"
+}
---
+++ python-launcher/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$targetDir = "$env:ProgramFiles\Python\PyLauncher"
+
+
+$msi = Get-Item "$toolsDir/*.msi"
+$msiArgs = "/quiet /qn /norestart REBOOT=ReallySupress ALLUSERS=1"
+Install-ChocolateyInstallPackage -PackageName $env:chocolateyPackageName `
+    -File $msi -FileType 'msi' -ValidExitCodes @( 0 ) -silentArgs $msiArgs
