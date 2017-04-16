ed37db54642ba356deade3f6da81fd8c75e7b9a6
Philipp Serr <episource@gmx.de>
2017-04-16 16:46:28 +0200

    Add pip to python2

--- python2/tools/chocolateyInstall.ps1
+++ python2/tools/chocolateyInstall.ps1
@@ -12,11 +12,16 @@ Start-Process -Wait msiexec @(
     "/a", $msi, "/passive", "/qn", "TARGETDIR=$toolsDir", "/liwe", "$toolsDir\msi.log"
 )
 
-# 3. Cleanup
+# 2. Install pip
+Start-Process -Wait -WindowStyle Hidden "$toolsDir\python.exe" `
+    @( "-E", "-s", "-m", "ensurepip", "-U", "--default-pip" ) 
+
+# 4. Cleanup
 Remove-Item -Recurse $tmpDir | Out-Null
 
-# 4. Add startmenu entry & Configure shimgen
+# 5. Add startmenu entry & Configure shimgen
 Install-StartMenuLink -LinkName "Python2" -TargetPath "$toolsDir\python.exe"
 Set-AutoShim -Pattern "**" -Mode Ignore | Out-Null
 Install-Shim -Name "python2" -Path "$toolsDir\python.exe"
 Install-Shim -Name "pythonw2" -Path "$toolsDir\pythonw.exe"
+Install-Shim -Name "pip2" -Path "$toolsDir\python.exe" -Arguments "-m pip"
