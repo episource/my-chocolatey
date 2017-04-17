d6ff1e156e0e0768e1a40b2e9a180ad55d65156d
Philipp Serr <episource@gmx.de>
2017-04-17 08:50:01 +0200

    Add package nirsoft-regdllview

---
+++ nirsoft-regdllview/_build.ps1
@@ -0,0 +1,41 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$startTitle = "RegDllView (x64)"
+$exeName = "RegDllView.exe"
+$zipUrl = "http://www.nirsoft.net/utils/regdllview-x64.zip"
+
+$changelogUrl = "http://www.nirsoft.net/utils/registered_dll_view.html"
+$changelogRaw = Invoke-WebRequest -UseBasicParsing $changelogUrl
+$changelogItems = $changelogRaw -split '(?=<li>Version)'
+$changes = $changelogItems[1..($changelogItems.Length-2)] | %{ $_ `
+    -replace "(?si)<li>(Version[^:`n`r]*).*<ul>",'= ${1} =' `
+    -replace "`r?`n(?!<li)(?!</ul>)",'' `
+    -replace '<li>',' * ' `
+    -replace '</ul>' 
+} | Out-String
+
+
+New-Package -VersionInfo @{
+    Version = "file:tools/$exeName"
+    ReleaseNotes = $changes
+} -PrepareFilesHook {
+    Import-PackageResource -Url $zipUrl -AutoUnzip
+    
+    Add-Content "$($_.BuildDir)/tools/chocolateyInstall.ps1" @"
+Set-StrictMode -Version latest
+`$ErrorAction = "Stop"
+
+
+`$toolsDir = "`$(Split-Path -Parent `$MyInvocation.MyCommand.Definition)"
+Install-StartMenuLink -LinkName "Nirsoft\Nirsoft $startTitle" -TargetPath "`$toolsDir/$exeName"
+Set-AutoShim -Pattern "**" -Mode Ignore | Out-Null
+"@
+} 
