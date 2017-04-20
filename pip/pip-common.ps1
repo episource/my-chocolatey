b72d54fe40e9f8bcd2849be9c82b4a691157dac3
Philipp Serr <episource@gmx.de>
2017-04-20 18:24:02 +0200

    Add first python pip packages

---
+++ pip/pip-common.ps1
@@ -0,0 +1,43 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+Import-Module Pip-Package
+
+
+function New-AutoPip() {
+    Set-StrictMode -Off
+    
+    $pkgId -match "^(?<PYTHON>python.*)-pip-(?<PKG>.*)$" | Out-Null
+    $pythonName = $Matches.PYTHON
+    $pypiName = $Matches.PKG
+
+
+    $pypiArgs = @{
+        PypiPackage = "$pypiName"
+        ChocoPackageMaintainer = "episource"
+    }
+    If ($maintainer) {
+        $pypiArgs.ChocoPackageMaintainer = $maintainer
+    }
+    If ($postInstallScript) {
+        $pypiArgs.PostInstallScript = $postInstallScript
+    }
+    If ($preUninstallScript) {
+        $pypiArgs.PreUninstallScript = $preUninstallScript
+    }
+    If ($additionalDependencies) {
+        $pypiArgs.AdditionalDependencies = $additionalDependencies
+    }
+
+
+    @( "$pythonName", "$pythonName-x86" ) | %{
+        $pythonInfo = Select-BuildDependency -Name $_ -Detailed
+        $pythonVer = $pythonInfo.Version
+        
+        $pypiArgs.PythonName = $_
+        $pypiArgs.PythonVersion = $pythonVer
+        New-PypiPackage @pypiArgs
+    }
+}
---
+++ pip/python3-pip-pickleshare/_build.ps1
@@ -0,0 +1,7 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+. $PSScriptRoot/../pip-common.ps1
+
+
+$pkgId = $(Get-Item $MyInvocation.MyCommand.Definition).Directory.Name
+New-AutoPip
