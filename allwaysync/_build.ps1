7c82c2fe700e4fab4d53ba893b2f3c1323537648
Philipp Serr <episource@gmx.de>
2017-11-18 16:26:36 +0100

    Add package allwaysync

---
+++ allwaysync/_build.ps1
@@ -0,0 +1,24 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+
+Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query download page
+$downloadPageUrl = "https://allwaysync.com/download/"
+$response = Invoke-WebRequest -UseBasicParsing -Uri $downloadPageUrl
+$href = $response.links | ?{ $_ -match "MSI 64-bit" } | %{ $_.href }
+
+# Extract version
+$href -match "allwaysync-x64-(?<VERSION>\d+-\d+-\d+)\.msi" | Out-Null
+$version = $Matches.Version -replace "-","."
+
+# Build the package
+@{
+    Version  = $version
+    FileUrl  = $href
+} | New-Package
---
+++ allwaysync/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+
+# Extract msi
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$msi      = Get-Item "$toolsDir\allwaysync-x64-*.msi"
+$pkg      = Move-Item $msi "$toolsDir\.." -PassThru
+
+Start-Process -Wait msiexec @(
+    "/a", $pkg, "/passive", "/qn", "TARGETDIR=$toolsDir",
+    "/liwe", "$toolsDir\msi.log"
+)
+
+Remove-Item $msi
+Remove-Item $pkg
+
+# Add startmenu entry
+$exe      = Join-Path $toolsDir "Bin\syncappw.exe"
+Install-StartMenuLink -LinkName "Allway Sync" -TargetPath $exe
+
+# Do not create any shims
+Set-AutoShim -Pattern "**" -Mode Ignore | Out-Null
