be8dd77b74a60a4f18d92d6cbc70f8726e99af6f
Philipp Serr <episource@gmx.de>
2018-01-17 21:58:18 +0100

    Repair automatic installation (pkg: npcap)
    Since npcap-0.97 silent installation is an exclusive feature of the
    commercial OEM-version. This commit adds code for automatically
    clicking through the installation wizard.
    
    I did not find an easy and reliable way to hide the installer window
    from the user. However, from the user's perspective, it will appear as
    status dialog, only.
    
    Gitlab issue: closes #23

--- npcap/tools/chocolateyInstall.ps1
+++ npcap/tools/chocolateyInstall.ps1
@@ -2,16 +2,42 @@
 $ErrorAction = "Stop"
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$exeInstaller = Get-Item "$toolsDir/npcap-*.exe"
+. $toolsDir/chocolateyInstallLib.ps1
+write-warning "The interactive npcap installer will open soon."
+write-warning "The chocolatey install script will go through the installer automatically."
 
-# /S : silent install
+
+# let installation timeout after a reasonable time
+$instTimeoutSec = 120
+
+# note: since 0.97 /S (silent) is supported by oem package, only!
 # npf_startup=yes : start driver at boot time
 # loopback_support=yes : capture loopback traffic
 # dlt_null=yes : don't fake ethernet frames when capturing loopback traffic - requires capturing software support (current wireshark supports this)
 # admin_only=no : all users can capture packets
 # dot11_support=yes : enable capturing of raw 802.11 (wlan) traffic (see https://wiki.wireshark.org/CaptureSetup)
 # vlan_support=yes : support capturing tagged ethernet frames
-# winpcap_mode=yes : install winpcap compatible interface
-$instArgs = "/S /npf_startup=yes /loopback_support=yes /dlt_null=yes /admin_only=no /dot11_support=yes /vlan_support=yes /winpcap_mode=yes"
-Install-ChocolateyInstallPackage -PackageName $env:chocolateyPackageName `
-    -File $exeInstaller -FileType 'exe' -silentArgs $instArgs 
+# winpcap_mode=no : use npcap native interface (no backwards compatibility)
+$instArgs = @( "/npf_startup=yes", "/loopback_support=yes", "/dlt_null=yes", "/admin_only=no", "/dot11_support=yes","/vlan_support=yes", "/winpcap_mode=no")
+
+$tStart = Get-Date
+$instProc = Start-Process -PassThru $exeInstaller -ArgumentList $instArgs
+
+# Fake silent installation by stepping through minimized installer dialog
+# => programmatically click confirmation button
+while (-not $instProc.HasExited -and $(Get-IsNoTimeout $tStart $instTimeoutSec)) {
+    $instProc.Refresh()
+    Click-Button $instProc @( "*yes*", "*agree*", "*install*", "*next*", "*finish*" ) | Out-Null
+    Start-Sleep 0.05
+}
+
+if (-not $instProc.HasExited) {
+    $instProc.Kill()
+    throw "Timeout - installation aborted!"
+} elseif ($instProc.ExitCode -ne 0) {
+    throw "Installation failed - non-zero exit code!"
+} else {
+    write-host "Npcap installed successfully!"
+}
+
+
---
+++ npcap/tools/chocolateyInstallLib.ps1
@@ -0,0 +1,124 @@
+﻿function Compile-PInvoke() {
+    $code = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Native {   
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ff468861(v=vs.85).aspx
+    public enum SystemDefinedMessage : uint {
+        WM_NEXTDLGCTL = 0x028,
+        WM_KEYDOWN    = 0x100,
+        WM_KEYUP      = 0x101,
+        WM_CHAR       = 0x102,
+        WM_COMMAND    = 0x0111,
+        BM_CLICK      = 0x0f5
+    }
+
+    public static class NativeMethods {   
+        private static StringBuilder buffer = new StringBuilder(10);
+    
+        public static bool IsEnabledButton(IntPtr hWnd) {
+            GetClassName(hWnd, buffer, buffer.Capacity);
+            return buffer.ToString().ToLowerInvariant() == "button"
+                && IsWindowEnabled(hWnd);
+        }
+        
+        public static string GetWindowText(IntPtr hWnd) {
+            GetWindowText(hWnd, buffer, buffer.Capacity);
+            return buffer.ToString();
+        }
+        
+        public static IList<IntPtr> EnumChildWindows(IntPtr hwndParent) {
+            var result = new List<IntPtr>();
+            EnumChildWindows(
+                hwndParent,
+                (hwnd, param) => {
+                    result.Add(hwnd); return true; 
+                },
+                IntPtr.Zero);
+            return result;
+        }
+        
+        
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        public static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+        
+        [DllImport("user32.dll")]
+        public static extern int GetDlgCtrlID(IntPtr hwndCtl);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName,int nMaxCount);
+        
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowEnabled(IntPtr hWnd);
+        
+        private delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+        
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+    }
+}
+"@
+
+    # Set up the compiler params, including any references required for the compilation
+    $codeProvider = New-Object Microsoft.CSharp.CSharpCodeProvider
+    $dependencies = @()
+    $compilerParams = New-Object System.CodeDom.Compiler.CompilerParameters(,$dependencies)
+
+    $compilationResult = $codeProvider.CompileAssemblyFromSource($compilerParams, $code);
+
+    # Force loading the generated assembly by accessing it
+    $assembly = $compilationResult.CompiledAssembly
+    $compilationResult.Errors | out-host
+}
+
+
+function Click-Button($proc, $textPatterns) {
+    $mainHwnd = [IntPtr]$proc.MainWindowHandle
+    $allHwnds = [Native.NativeMethods]::EnumChildWindows($mainHwnd)
+    $textPatternList = @() + $textPatterns
+    
+    foreach ($hwnd in $allHwnds) {
+        if (-not [Native.NativeMethods]::IsEnabledButton($hwnd)) {
+            continue;
+        }
+        
+        $text = [Native.NativeMethods]::GetWindowText($hwnd)
+        if ([String]::IsNullOrEmpty($text)) {
+            continue;
+        }
+        
+        foreach ($pattern in $textPatternList) {
+            if ($text -like $pattern) {
+                
+                # code 0 works well
+                $notificationCode = 0
+                $ctrlId = [Native.NativeMethods]::GetDlgCtrlID($hwnd)
+                
+                $wParam = ($notificationCode -shl 16) -bor $ctrlId
+                $lParam = $hwnd.ToInt32()
+                return [Native.NativeMethods]::PostMessage(
+                    [IntPtr]$mainHwnd,
+                    [Native.SystemDefinedMessage]::WM_COMMAND,
+                    $wParam, $lParam)
+            }
+        }
+    }
+    
+    return $false
+}
+
+function Get-IsNoTimeout($tStart, $timeoutSec) {
+    return $($(Get-Date) - $tStart).TotalSeconds -le $timeoutSec
+}
+
+
+Compile-PInvoke
+$exeInstaller = Get-Item "$toolsDir/npcap-*.exe"
