80bb672db14069c0dd7bfe56fcd2e57ff1f741eb
Philipp Serr <episource@gmx.de>
2018-01-28 17:35:13 +0100

    Add package vlc-x86

---
+++ vlc-x86/_build.ps1
@@ -0,0 +1,42 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+$fileListUrl = "http://download.videolan.org/vlc/last/win32"
+$fileList = Invoke-Webrequest -UseBasicParsing $fileListUrl
+
+$zipRegex = "vlc-(?<VERSION>\d+(?:\.\d+){1,2})-win32\.zip"
+$zipUrl = $null
+$sha256Url = $null
+$version = $null
+
+ForEach ($link in $fileList.Links) {
+    $href = $link.href
+    
+    If ($href -match "$zipRegex$") {
+        $zipUrl = "$fileListUrl/$href"
+        $version = $Matches.VERSION
+    }
+    
+    If ($href -match "$zipRegex\.sha256$") {
+        $sha256Url = "$fileListUrl/$href"
+        $version = $Matches.VERSION
+    }
+}
+
+If (-not $zipUrl -or -not $sha256Url -or -not $version) {
+    Throw "Failed to retrieve version information."
+}
+
+
+$sha256Response = Get-ChecksumFromWeb -Url $sha256Url -Filename '.*' -EnableRegex
+$sha256 = "sha256:$($sha256Response.Checksum)"
+
+
+New-Package @{
+    Version = $version
+    FileUrl = $zipUrl
+    Checksum = $sha256
+}
