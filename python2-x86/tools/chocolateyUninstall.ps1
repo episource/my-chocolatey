d5bfeff5ef0c9b9e44971e2f53a6e27cd79b8f49
Philipp Serr <episource@gmx.de>
2018-06-16 14:09:57 +0200

    Drop support for python packages
    Gitlab issue: closes #18, #30

--- python2-x86/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Set-StrictMode -Version latest
-$ErrorAction = "Stop"
-$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-
-
-Start-Process -Wait -WindowStyle Hidden "$toolsDir\python.exe" `
-    @( "-E", "-s", "-B", "-m", "ensurepip._uninstall" )
