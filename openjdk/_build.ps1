6c3023c5f80dfc94ec574391211433e1b3c2660a
Philipp Serr <episource@gmx.de>
2019-08-08 12:21:00 +0200

    Replace oraclejdk with openjdk

---
+++ openjdk/_build.ps1
@@ -0,0 +1,26 @@
+﻿# Enable common parameters
+[CmdletBinding()] Param()
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query github API to get url and version of the latest release
+$repo = 'ojdkbuild/ojdkbuild'
+$filename = "java-[0-9]+-openjdk-(?<VERSION>[0-9\.]+-[0-9]).windows\.ojdkbuild\.x86_64\.zip"
+$hashfile = "$filename\.sha256"
+
+
+# Export the package (subject to _config.ps1)
+$vi = Get-VersionInfoFromGithub -Repo $repo -File $filename -EnableRegex -Limit 10 -FindMax |
+      Add-ChecksumFromGithubAsset -ChecksumFileRegex $hashfile -Algorithm sha256
+      
+# Massage the version identifier
+# Chocolatey uses a flavored semver style
+#  - "-" separated sufix introduces prerelease information
+# Openjdk uses "-" sufix for build information
+#  => merge revision and build number
+$semverTokens = Get-SemverTokens $vi.Version -DefaultMajorMinorPatch
+$revision = [int]$semverTokens.REVISION * 100 + [int]$semverTokens.PRERELEASE
+$vi.Version = "$($semverTokens.MAJOR).$($semverTokens.MINOR).$($semverTokens.PATCH).$revision"
+
+New-Package -VersionInfo $vi -AutoUnzip:$false
---
+++ openjdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,57 @@
+﻿Set-StrictMode -Version latest
+$ErrorAction = "Stop"
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# Extract JDK
+$jdkZip = Get-Item "$toolsDir/java-*.zip"
+Get-ChocolateyUnzip -FileFullPath $jdkZip.FullName -Destination $toolsDir
+Remove-Item $jdkZip
+
+$jdkName = $(Get-Item "$toolsDir/java-*").Name
+$jdkDir = $(Get-Item "$toolsDir/java-*").FullName
+
+# Only create shims for the files listed in $withDefaultShim
+$withDefaultShim = @(
+    "$jdkName\bin\javac.exe",
+    "$jdkName\bin\jconsole.exe",
+    "$jdkName\bin\jdb.exe",
+    "$jdkName\bin\java.exe",
+    "$jdkName\bin\javaw.exe"
+)
+Set-AutoShim -Pattern $withDefaultShim -Invert -Mode Ignore | Out-Null
+
+# Install start menu shortcuts for jmc + jvisualvm
+$startJconsole = @{ LinkName="Java/Java Monitoring & Management Console (JConsole)"; TargetPath="$jdkDir\bin\jconsole.exe" }
+Install-StartMenuLink @startJconsole
+
+# Setup JAVA_HOME
+Install-ChocolateyEnvironmentVariable -VariableType "Machine" `
+    -VariableName "JAVA_HOME" -VariableValue "$jdkDir"
+    
+# Register JDK to be found by launchers looking at oracles registry paths
+# (e.g. launch4j)
+$versionOut = & "$jdkDir/bin/java.exe" -version 2>&1
+$versionOut[0] -match "^openjdk version ""(?<INTERNAL_VERSION>[^""]+)""" | Out-Null
+$internalVersion = $Matches.INTERNAL_VERSION
+
+$jdk = @{
+    "HKLM:\SOFTWARE\JavaSoft\Java Development Kit" = @{
+        "CurrentVersion" = $internalVersion;
+        "$internalVersion" = @{
+            "JavaHome" = $jdkDir;
+            "RuntimeLib" = "$jdkDir\bin\server\jvm.dll"
+        }
+    }
+}
+Install-RegistryImage -Force $jdk
+
+# Associate *.jar with javaw.exe 
+# (global HKCR associaton, might be overwritten by HKCU:SOFTWARE\Classes\.jar)
+$ftype = "jar_jdk-$internalVersion"
+& cmd /c "ftype $ftype=""$jdkDir\bin\javaw.exe"" -jar ""%1"" ""%~1"""
+& cmd /c "assoc .jar=$ftype"
+
+$uninstallScript = "$jdkDir/chocolateyUninstall.ps1"
+Add-Content $uninstallScript -Value "`n& cmd /c ""ftype $ftype="""
+Add-Content $uninstallScript -Value "`n& cmd /c ""assoc .jar="""
