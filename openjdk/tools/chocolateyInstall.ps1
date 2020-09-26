8c35a55b962a4e2b4ad2db43bb1879c0fe698f17
Philipp Serr <episource@gmx.de>
2020-09-26 17:53:47 +0200

    Fix openjdk installation

--- openjdk/tools/chocolateyInstall.ps1
+++ openjdk/tools/chocolateyInstall.ps1
@@ -8,30 +8,34 @@ $jdkZip = Get-Item "$toolsDir/java-*.zip"
 Get-ChocolateyUnzip -FileFullPath $jdkZip.FullName -Destination $toolsDir
 Remove-Item $jdkZip
 
-$jdkName = $(Get-Item "$toolsDir/java-*").Name
-$jdkDir = $(Get-Item "$toolsDir/java-*").FullName
+$jdkName = $(Get-Item "$toolsDir/java-*")[-1].Name
+$jdkDir = $(Get-Item "$toolsDir/java-*")[-1].FullName
+
+# Move jdkDir content directly to tools dir
+Get-Item "$jdkDir/*" | %{ Move-Item $_ $toolsDir } | Out-Null
+Remove-Item $jdkDir
 
 # Only create shims for the files listed in $withDefaultShim
 $withDefaultShim = @(
-    "$jdkName\bin\javac.exe",
-    "$jdkName\bin\jconsole.exe",
-    "$jdkName\bin\jdb.exe",
-    "$jdkName\bin\java.exe",
-    "$jdkName\bin\javaw.exe"
+    "$toolsDir\bin\javac.exe",
+    "$toolsDir\bin\jconsole.exe",
+    "$toolsDir\bin\jdb.exe",
+    "$toolsDir\bin\java.exe",
+    "$toolsDir\bin\javaw.exe"
 )
 Set-AutoShim -Pattern $withDefaultShim -Invert -Mode Ignore | Out-Null
 
 # Install start menu shortcuts for jmc + jvisualvm
-$startJconsole = @{ LinkName="Java/Java Monitoring & Management Console (JConsole)"; TargetPath="$jdkDir\bin\jconsole.exe" }
+$startJconsole = @{ LinkName="Java/Java Monitoring & Management Console (JConsole)"; TargetPath="$toolsDir\bin\jconsole.exe" }
 Install-StartMenuLink @startJconsole
 
 # Setup JAVA_HOME
 Install-ChocolateyEnvironmentVariable -VariableType "Machine" `
-    -VariableName "JAVA_HOME" -VariableValue "$jdkDir"
+    -VariableName "JAVA_HOME" -VariableValue "$toolsDir"
     
 # Register JDK to be found by launchers looking at oracles registry paths
 # (e.g. launch4j)
-$versionOut = & "$jdkDir/bin/java.exe" -version 2>&1
+$versionOut = & "$toolsDir/bin/java.exe" -version 2>&1
 $versionOut[0] -match "^openjdk version ""(?<INTERNAL_VERSION>[^""]+)""" | Out-Null
 $internalVersion = $Matches.INTERNAL_VERSION
 
@@ -39,8 +43,8 @@ $jdk = @{
     "HKLM:\SOFTWARE\JavaSoft\Java Development Kit" = @{
         "CurrentVersion" = $internalVersion;
         "$internalVersion" = @{
-            "JavaHome" = $jdkDir;
-            "RuntimeLib" = "$jdkDir\bin\server\jvm.dll"
+            "JavaHome" = $toolsDir;
+            "RuntimeLib" = "$toolsDir\bin\server\jvm.dll"
         }
     }
 }
@@ -49,9 +53,9 @@ Install-RegistryImage -Force $jdk
 # Associate *.jar with javaw.exe 
 # (global HKCR associaton, might be overwritten by HKCU:SOFTWARE\Classes\.jar)
 $ftype = "jar_jdk-$internalVersion"
-& cmd /c "ftype $ftype=""$jdkDir\bin\javaw.exe"" -jar ""%1"" ""%~1"""
+& cmd /c "ftype $ftype=""$toolsDir\bin\javaw.exe"" -jar ""%1"" ""%~1"""
 & cmd /c "assoc .jar=$ftype"
 
-$uninstallScript = "$jdkDir/chocolateyUninstall.ps1"
+$uninstallScript = "$toolsDir/chocolateyUninstall.ps1"
 Add-Content $uninstallScript -Value "`n& cmd /c ""ftype $ftype="""
 Add-Content $uninstallScript -Value "`n& cmd /c ""assoc .jar="""
