c8ee798a9cdc033f741af0222d3c977401133790
Philipp Serr <episource@gmx.de>
2022-03-05 09:22:02 +0100

    Keep installing git via chocolatey

---
+++ git/_build.ps1
@@ -0,0 +1,27 @@
+# Enable common parameters
+[CmdletBinding()] Param()
+# Import my-chocolatey config & modules
+. $PSScriptRoot/../_root.ps1
+
+
+# Query github API to get url and version of the latest release
+$repo = 'git-for-windows/git'
+$filename = "PortableGit-[0-9\.]+-64-bit\.7z\.exe"
+
+# Export the package (subject to _config.ps1)
+Get-VersionInfoFromGithub -Repo $repo `
+    -File $filename -EnableRegex `
+    -ExtractVersionHook {
+        $_.tag_name -match "^v(?<MAIN_VERSION>\d+.\d+.\d+)\.windows\.(?<SUB_VERSION>\d+)" | Out-Null
+        If ($Matches.SUB_VERSION -gt 1) {
+            return "$($Matches.MAIN_VERSION).$($Matches.SUB_VERSION)"
+        }
+        return $Matches.MAIN_VERSION
+    } |
+Add-ChecksumFromGithubRelease -GetChecksumHook {
+        param($ghRelease, $fname, $fnameEscaped)
+        $ghRelease.body -match `
+            "(?m)$fnameEscaped\s*\|\s*(?<CHECKSUM>[a-fA-F0-9]{64})" | Out-Null
+        return "sha256:$($Matches.CHECKSUM)"
+    } |
+New-Package
