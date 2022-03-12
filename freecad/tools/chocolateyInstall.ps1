a7a12f5b6f3457a1c255e2c063331a538e332df4
Philipp Serr <episource@gmx.de>
2022-03-12 13:55:20 +0100

    Fix freecad build

--- freecad/tools/chocolateyInstall.ps1
+++ freecad/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $ErrorAction = "Stop"
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 
-$installerExe = Get-Item "$toolsDir/*-installer1?.exe"
+$installerExe = Get-Item "$toolsDir/*-installer*.exe"
 Get-ChocolateyUnzip -FileFullPath $installerExe.FullName -Destination $toolsDir
 Remove-Item $installerExe
-Remove-Item "$toolsDir/uninstall.exe"
+Remove-Item "$toolsDir/Uninstall-FreeCAD.exe"
 Remove-Item -Recurse "$toolsDir/`$PLUGINSDIR"
 
 $exePath = "$toolsDir/bin/FreeCAD.exe"
