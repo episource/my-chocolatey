823ef7e3582b5327e10193ff32af48d9992ec1f6
Philipp Serr <episource@gmx.de>
2022-12-27 14:27:29 +0100

    Fix extraction of 7+ taskbar tweaker setup

--- 7plus-taskbar-tweaker/tools/chocolateyInstall.ps1
+++ 7plus-taskbar-tweaker/tools/chocolateyInstall.ps1
@@ -2,11 +2,10 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 Set-Location $toolsDir
 
 $installer = Get-Item "7tt_setup.exe"
-Get-ChocolateyUnzip -FileFullPath $installer.FullName -Destination $toolsDir
+Get-ChocolateyUnzip -FileFullPath $installer.FullName -SpecificFolder "bin/64" -Destination $toolsDir
 Move-Item -Force "bin/64/7+ Taskbar Tweaker.ex2" "$toolsDir/7+ Taskbar Tweaker.exe"
 Move-Item -Force "bin/64/inject.dll" "$toolsDir/inject.dll"
 Remove-Item -Recurse bin
-Remove-Item -Recurse '$PLUGINSDIR'
 Remove-Item $installer
 
 Install-StartMenuLink -LinkName "7+ Taskbar Tweaker" `
