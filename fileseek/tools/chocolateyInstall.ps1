ccbe8baa90d7fb17d3ffb85d88987760faa313ea
Philipp Serr <episource@gmx.de>
2023-12-29 15:26:32 +0100

    Update fileseek directory path

--- fileseek/tools/chocolateyInstall.ps1
+++ fileseek/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $startmenu = @{
     LinkName   = "FileSeek"
-    TargetPath = "$toolsDir\FileSeek\FileSeek.exe"
+    TargetPath = "$toolsDir\FileSeek64\FileSeek.exe"
 }
 Install-StartMenuLink @startmenu
